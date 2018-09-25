94e1d63c6d547c698bc75dccf0046a608e079db7
Gleb Molkov <crazmuss@gmail.com>
2018-09-25 16:32:03 +0300

    added templates

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -53,7 +53,9 @@ ROOT_URLCONF = 'geekshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'geekshop/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
